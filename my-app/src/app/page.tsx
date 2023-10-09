2804a4bad97548d5bbc96511b95ee3074fec0075
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-10-09 16:32:01 +0200

    refactoring dev branch

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,12 +1,9 @@
 import Image from "next/image";
-import logoFirst from "../../public/logos1.png";
-import logoSecond from "../../public/logos3.png";
-import DesignSystem from "../../pages/DesignSystem";
+
 
 export default function Home() {
   return (
     <div>
-      <DesignSystem />
       <hr />
     </div>
   );
