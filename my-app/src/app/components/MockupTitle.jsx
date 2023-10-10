b18c460f2339dc982e67bcb045b9cab64af4aa3f
Joele-E <esposito.joelebiz@gmail.com>
2023-10-10 16:12:05 +0200

    feat: added mockup title

---
+++ my-app/src/app/components/MockupTitle.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Typography from "@mui/joy/Typography";
+
+const MockupTitle = () => {
+  return (
+    <Typography
+      /* color="primary" */
+      level="h1"
+      noWrap={false}
+      variant="plain"
+      className="text-center text-6xl text-gray-300 content-center"
+    >
+      Discover our features!
+    </Typography>
+  );
+};
+
+export default MockupTitle;
