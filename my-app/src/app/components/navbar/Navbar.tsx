8fc3144509e2124b79984593533b9c401a0283d6
Jef <jefersonrevellobiz@gmail.com>
2023-10-11 23:51:33 +0200

    center svg flags inside select dropdown

--- my-app/src/app/components/navbar/Navbar.tsx
+++ my-app/src/app/components/navbar/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
   return (
     <div className="z-10">
       <div className="navbar relative flex items-center p-3 justify-between">
-        <div className="navbar-background absolute bg-black opacity-10 h-full w-full p-0 m-0 "></div>
+        <div className="navbar-background absolute bg-black opacity-20 h-full w-full p-0 m-0 left-0 shadow-md shadow-zinc-500"></div>
         <div className="flex items-center ml-8">
           {/* Cliccare sul logo riporta alla home page */}
           <Link href="/" className="mr-4 flex items-center z-50">
@@ -29,39 +29,57 @@ export default function Navbar() {
             Design System
           </Link>
         </div>
+        <div className="flex mr-4">
+          <div className="flex mr-8">
+            <Link href="" className="mx-4 flex items-center z-50">
+              Sign Up
+            </Link>
+            <Pulsante content="Log in" color="" colorhover=""></Pulsante>
+          </div>
 
-        <div className="flex mr-8">
-          <Link href="" className="mx-4 flex items-center z-50">
-            Sign Up
-          </Link>
-          <Pulsante content="Log in" color="" colorhover=""></Pulsante>
-        </div>
-
-        <Select
-          inputProps={{
-            IconComponent: () => null,
-            MenuProps: {
-              MenuListProps: {
-                sx: {
-                  backgroundColor: "black",
+          <Select
+            inputProps={{
+              IconComponent: () => null,
+              MenuProps: {
+                PaperProps: {
+                  sx: {
+                    backgroundColor: "transparent", // Set the background color to transparent
+                  },
+                },
+                MenuListProps: {
+                  sx: {
+                    width: "65px",
+                  },
                 },
               },
-            },
-          }}
-          value={language}
-          label="Language"
-          onChange={handleLanguageChange}
-          className="text-white"
-          variant="standard"
-          disableUnderline
-        >
-          <MenuItem value={"en"}>
-            <Image alt="en" src="/gb.svg" width="28" height="28" />
-          </MenuItem>
-          <MenuItem value={"it"}>
-            <Image alt="en" src="/it.svg" width="28" height="28" />
-          </MenuItem>
-        </Select>
+            }}
+            value={language}
+            label="Language"
+            onChange={handleLanguageChange}
+            className="text-white relative border-0"
+            variant="standard"
+            disableUnderline
+          >
+            <MenuItem value={"en"} className="mt-2">
+              <Image
+                alt="en"
+                src="/gb.svg"
+                width="28"
+                height="28"
+                className="absolute left-1/2"
+              />
+            </MenuItem>
+            <MenuItem value={"it"} className="mt-6">
+              <Image
+                alt="en"
+                src="/it.svg"
+                width="28"
+                height="28"
+                className="absolute left-1/2"
+              />
+            </MenuItem>
+          </Select>
+        </div>
       </div>
     </div>
   );
