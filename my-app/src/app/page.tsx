f922aa298e6fcb2dbcc98284d51c0501b52bb600
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-10-13 17:14:18 +0200

    merged banner Mockup

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,27 +1,31 @@
 import Image from "next/image";
 import Navbar from "./components/navbar/Navbar";
+import MockupTitle from "./components/MockupTitle";
 
 export default function Home() {
   return (
-    <div className=" text-white font-body h-screen text-default m-0">
-      <div className="background-image absolute h-screen w-screen m-0 p-0 -z-10">
-        <Image
-          src="/background top image.jpg"
-          alt=""
-          height="5865"
-          width="3915"
-          className="w-full h-full m-0 p-0"
-        />
-      </div>
+    <div>
+      <div className=" text-white font-body h-screen text-default m-0">
+        <div className="background-image absolute h-screen w-screen m-0 p-0 -z-10">
+          <Image
+            src="/background top image.jpg"
+            alt=""
+            height="5865"
+            width="3915"
+            className="w-full h-full m-0 p-0"
+          />
+        </div>
 
-      <Navbar />
-      {/* TITLES */}
-      <div className="section-homepage w-1/2 mt-5 p-8">
-        <div className="title-homepage">
-          <h1 className="text-header">Where music meets collaboration</h1>
-          <h2 className="text-sub-header mt-7">Join the revolution</h2>
+        <Navbar />
+        {/* TITLES */}
+        <div className="section-homepage w-1/2 mt-5 p-8">
+          <div className="title-homepage">
+            <h1 className="text-header">Where music meets collaboration</h1>
+            <h2 className="text-sub-header mt-7">Join the revolution</h2>
+          </div>
         </div>
       </div>
+      <MockupTitle/>
     </div>
   );
 }
