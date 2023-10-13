a6213d96b2f133bf913357daab90b514b17a528a
Joele-E <esposito.joelebiz@gmail.com>
2023-10-13 12:27:35 +0200

    feat: added mockup section

---
+++ my-app/src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroMockup from "../components/hero-mockup/HeroMockup";
+
+const Test = () => {
+  return (
+    <div>
+      <HeroMockup />
+    </div>
+  );
+};
+
+export default Test;
