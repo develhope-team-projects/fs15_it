d5884d51bfe0431197496b2d238d84f1e2d5ccb6
AntonioIannibelli31 <antonioiannibelli31@gmail.com>
2023-11-13 16:31:32 +0100

    .

--- my-app/src/app/db/db.mjs
+++ my-app/src/app/db/db.mjs
@@ -1,11 +1,13 @@
 import pgPromise from "pg-promise";
 import "dotenv/config";
 
-const { DB }: any = process.env;
-const db = pgPromise()(DB);
+// const { DB }: any = process.env;
+const db = pgPromise()(
+  "postgres://postgres:postgres@localhost:5432/sound-bridge"
+);
 const setupDb = () => {
   db.none(
-    `CREATE TABLE IF NOT EXISTS users(
+    `CREATE TABLE users (
         id SERIAL PRIMARY KEY,
         username TEXT NOT NULL,
         password TEXT NOT NULL,
