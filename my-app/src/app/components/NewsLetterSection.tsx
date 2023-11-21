1bdc240d7d30ca6ba6a0b23d620da58100691a2f
AntonioIannibelli31 <antonioiannibelli31@gmail.com>
2023-11-21 20:17:58 +0100

    added input newsletter

--- my-app/src/app/components/NewsLetterSection.tsx
+++ my-app/src/app/components/NewsLetterSection.tsx
@@ -1,6 +1,7 @@
 import InputField from "./Input";
 import Btn from "./Btn";
 import translations from "@/app/translations";
+import Input from "./newInput/input";
 interface NewsletterSectionProps {
   language: string;
 }
@@ -12,8 +13,8 @@ export default function NewsLetterSection({
     <div className=" flex flex-col gap-2  justify-center items-center p-12">
       <h3 className="font-body text-white">{t[language].newsletter.header}</h3>
       <div className="flex gap-2 justify-center items-center max-sm:flex-col ">
-        <InputField />
-        <Btn content={t[language].newsletter.done} />
+        <Input />
+        {/* <Btn content={t[language].newsletter.done} /> */}
       </div>
     </div>
   );
