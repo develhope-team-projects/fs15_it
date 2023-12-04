29b899cc1f94cc24a12c108281b58b2a1f3edc27
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-12-04 18:30:46 +0100

    Fixed some bugs in the form

--- my-app/src/app/components/form/form.jsx
+++ my-app/src/app/components/form/form.jsx
@@ -1,95 +1,80 @@
 import { Card } from "@mui/joy";
 import { useRouter } from "next/navigation";
-import {
-  Button,
-  CardContent,
-  Grid,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-} from "@mui/material";
+// import {
+//   Button,
+//   CardContent,
+//   Grid,
+//   InputLabel,
+//   MenuItem,
+//   Select,
+//   TextField,
+// } from "@mui/material";
 import { useState } from "react";
 export default function Form() {
   const router = useRouter();
   const sendUser = async (e) => {
     e.preventDefault();
-    try {
-      await fetch("/api/user", {
+
+    if (name && password) {
+      const res = await fetch("/api/user", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ name, password, role }),
       });
-      if (role === "singer") {
+      if (res.status == 201 && role === "singer") {
         router.push("/artists");
-      } else {
+      } else if (res.status == 201 && role === "beatmaker") {
         router.push("/beatmakers");
+      } else {
+        alert("Name already in use");
       }
-    } catch (err) {
-      console.log(err);
+    } else {
+      alert("Insert some valid credentials");
     }
   };
 
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
+  const [name, setName] = useState();
+  const [password, setPassword] = useState();
   const [role, setRole] = useState("singer");
 
   return (
-    <Card style={{ maxWidth: 450, margin: "0 auto" }}>
-      <CardContent>
-        <form>
-          <Grid container spacing={1}>
-            <Grid xs={12} item sm={6}>
-              <TextField
-                label="email"
-                placeholder="Enter your email *"
-                fullWidth
-                required
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                type="email"
-              ></TextField>
-            </Grid>
-            <Grid xs={12} item sm={6}>
-              <TextField
-                label="password"
-                placeholder="Enter your password *"
-                fullWidth
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                type="password"
-              ></TextField>
-            </Grid>
-            <Grid xs={12} item sm={6}>
-              <InputLabel id="demo-simple-select-label">Role *</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                label="role "
-                value={role}
-                onChange={(e) => setRole(e.target.value)}
-                required
-              >
-                <MenuItem value="singer">Singer</MenuItem>
-                <MenuItem value="beatmaker">Beatmaker</MenuItem>
-              </Select>
-            </Grid>
-            <Grid xs={12} item>
-              <Button
-                type="button"
-                onClick={sendUser}
-                variant="contained"
-                fullWidth
-              >
-                Submit
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </CardContent>
-    </Card>
+    <form>
+      <input
+        label="email"
+        placeholder="Enter your email *"
+        required
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        type="email"
+      />
+      <input
+        label="password"
+        placeholder="Enter your password *"
+        fullWidth
+        required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+      />
+      <label htmlFor="demo-simple-select-label">Role </label>
+      <select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        label="role "
+        value={role}
+        onChange={(e) => setRole(e.target.value)}
+        placeholder=""
+        required
+      >
+        <option value="singer">Singer</option>
+        <option value="beatmaker">Beatmaker</option>
+      </select>
+
+      <button type="submit" onClick={sendUser}>
+        Submit
+      </button>
+    </form>
   );
 }
